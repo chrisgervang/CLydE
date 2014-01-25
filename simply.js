c07c7e8c40f1b5d9c10cc760340d4514ab5ed69e
ImJohnDoe <Pshichenko@hotmail.com>
2014-01-25 15:18:53 -0800

    Update simply.js

--- simply.js
+++ simply.js
@@ -1,1 +1,31 @@
+/*
 simply.title('Hello World!');
+
+var controller = {};
+
+controller.doSomething = function(x) {
+  simply.subtitle(x);
+};
+
+return controller;
+
+var config = {
+  setting: 'Setting'
+};
+
+return config;
+
+controller.doSomething(config.setting);
+*/
+var count = parseInt(localStorage.getItem('count')) || 0;
+
+simply.on('singleClick', function(e) {
+  if (e.button === 'up') {
+    simply.subtitle(++count);
+  } else if (e.button === 'down') {
+    simply.subtitle(--count);
+  }
+  localStorage.setItem('count', count);
+});
+
+simply.text({ title: 'Counter', subtitle: count });
