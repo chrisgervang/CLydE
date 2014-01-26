c1b64cc482553cc3ca327d43d46d40ef7dc6db67
ImJohnDoe <Pshichenko@hotmail.com>
2014-01-25 23:27:17 -0800

    Update simply.js

--- simply.js
+++ simply.js
@@ -23,25 +23,23 @@ simply.on('singleClick', function(e) {
   if ((e.button === 'up') && (home === 0)) {
     changeState(1);
     } 
-  if ((e.button === 'down') && (home === 0)){
+  if ((e.button === 'down') && (home === 0)) {
       changeState(0);
     }
-  if ((e.button === 'select') && (home === 0))  {
+  if ((e.button === 'select') && (home === 0)) {
       if (state === 1) {
         home = 1;
-        textArr = {title: 'Curtains', subtitle: ' ', body: 'Toggle curtains'};
+        textArr = {title: 'Curtains', body: 'Toggle curtains'};
         simply.text(textArr);
-        simply.on('singleClick', function(g) {
-          if ((g.button === 'select') && (home === 1)) {
+          if ((e.button === 'select') && (home === 1)) {
             simply.subtext((curtains+1)%2);
           }
-          if (((g.button === 'up') && (home === 1)) || ((g.button === 'down') && (home === 1))) {
+          if (((e.button === 'up') && (home === 1)) || ((e.button === 'down') && (home === 1))) {
             state = 2;
             simply.text(textArr);
             home = 0;
           }
         });
-      }
       if (state === 2) {
         textArr = {title: 'Lights', subtitle: ' ', body: 'Toggle lights'};
         simply.text(textArr);
