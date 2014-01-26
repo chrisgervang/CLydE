e295f6ca412c8e46918ee7c5d0bd941ed643c1e1
Chris Gervang <gervangster@live.com>
2014-01-26 09:58:11 -0800

    merge

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-//var spark = require('sparkcloud')('ac427016082e1adc9172f7e6c32e810a26bcc6ebd8', 30000);
-//spark.access_token = "access_token=ac427016082e1adc9172f7e6c32e810a26bcc6ebd8";
+var request = require('request');
 
-//var core = spark.device('53ff6c065067544829381287');
+
+var util = require('util');
+var exec = require('child_process').exec;
+
+voiceURL = "http://localhost:59125/process?INPUT_TEXT=Goodmorning%20sir.%20The%20time%20is%209%20am,%20and%20its%20time%20to%20get%20up!%20Your%20first%20event%20is%20with%20Ryan%20at%20CoHo%20in%20one%20hour?&INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&AUDIO=WAVE_FILE&LOCALE=en_GB";
 
 var app = express();
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -35,43 +39,122 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+app.get('/curtain', function(request, response){
+	response.send("opening shades");
+
+	var command = 'curl --data "motorOn" https://api.spark.io/v1/devices/48ff6e065067555050192387/motorOn?access_token=427016082e1adc9172f7e6c32e810a26bcc6ebd8';
+	child = exec(command, function(error, stdout, stderr){
+
+		console.log('stdout: ' + stdout);
+		console.log('stderr: ' + stderr);
+
+		if(error !== null)
+		{
+		    console.log('exec error: ' + error);
+		}
+
+	});
+});
+
+var lightState = false;
+
+
+//
+app.get('/toggleLights', function(request, response){
+	response.send("toggleing lights");
+
+	speak("Let's toggle the lights!");
+	
+	var command;
+
+	if (lightState) {
+		command = 'curl --data "args=0,20,A" https://api.spark.io/v1/devices/48ff6e065067555050192387/fade?access_token=427016082e1adc9172f7e6c32e810a26bcc6ebd8';
+		lightState = false;
+	} else {
+		command = 'curl --data "args=255,20,A" https://api.spark.io/v1/devices/48ff6e065067555050192387/fade?access_token=427016082e1adc9172f7e6c32e810a26bcc6ebd8';
+		lightState = true;
+	}
+	
+
+	child = exec(command, function(error, stdout, stderr){
+		console.log('stdout: ' + stdout);
+		console.log('stderr: ' + stderr);
+		if(error !== null) {
+		    console.log('exec error: ' + error);
+		}
+	});
+});
+
+var lightDim = 0;
+
+app.get('/dimLights', function(request, response){
+	response.send("dimming lights");
+
+	//255,20,A <-request
+	var config = request.query['params'].split(',');
+	console.log(config);
+
+
+
+	if (config[0] < lightDim) {
+		//Hold on a second while I set the mood. Dimming the lights to {config[0]}
+  		speak("Hold on a second while I set the mood. Dimming the lights to " + config[0]);
+	} else if (config[0] > lightDim) {
+		//
+		speak("Hold on a second while I set the mood. Dimming the lights to " + config[0]);
+	}
+	//speak("POPOPPOPOPOPOPOP");
+	var command = 'curl --data "args='+ request.query['params'] +'" https://api.spark.io/v1/devices/48ff6e065067555050192387/fade?access_token=427016082e1adc9172f7e6c32e810a26bcc6ebd8';
+	child = exec(command, function(error, stdout, stderr){
+		console.log('stdout: ' + stdout);
+		console.log('stderr: ' + stderr);
+		if(error !== null) {
+		    console.log('exec error: ' + error);
+		}
+	});
+});
+
+
+function speak(words) {	
+
+	var command = 'osascript /Users/chrisgervang/CLydE/speak.scpt "' + words + '"';
+	console.log(words);
+
+	child = exec(command, function(error, stdout, stderr){
+		console.log('stdout: ' + stdout);
+		console.log('stderr: ' + stderr);
+		if(error !== null) {
+		    console.log('exec error: ' + error);
+		}
+	});
+
+}
+
+
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
 
-var util = require('util');
-var exec = require('child_process').exec;
 
-var command = 'curl --data "args=220,10,A" https://api.spark.io/v1/devices/48ff6e065067555050192387/fade?access_token=427016082e1adc9172f7e6c32e810a26bcc6ebd8';
+// var command = 'curl --data "args=230,50,A" https://api.spark.io/v1/devices/48ff6e065067555050192387/fade?access_token=427016082e1adc9172f7e6c32e810a26bcc6ebd8';
+// //var command = 'curl --data "motorOn" https://api.spark.io/v1/devices/48ff6e065067555050192387/motorOn?access_token=427016082e1adc9172f7e6c32e810a26bcc6ebd8';
 
-child = exec(command, function(error, stdout, stderr){
 
-console.log('stdout: ' + stdout);
-console.log('stderr: ' + stderr);
+// child = exec(command, function(error, stdout, stderr){
 
-if(error !== null)
-{
-    console.log('exec error: ' + error);
-}
+// 	console.log('stdout: ' + stdout);
+// 	console.log('stderr: ' + stderr);
 
-});
+// 	if(error !== null)
+// 	{
+// 	    console.log('exec error: ' + error);
+// 	}
 
-// var spawn = require('child_process').spawn,
-//     curl = spawn('curl --data', ['https://api.spark.io/v1/devices/48ff6e065067555050192387/motorOn?access_token=427016082e1adc9172f7e6c32e810a26bcc6ebd8'])
-//     data = '',
-// 	err  = null;
+// });
 
-// curl.stdout.on('data', function(chunk) {
-// 	    data += chunk;
-// 	});
- 
-// 	curl.stderr.on('data', function(err_msg) {
-// 	    if (err === null) { err = ''; }
-// 		err += err_msg;
-// 	});
- 
-// 	curl.on('exit', function(exit_code) {
-// 	    console.log(err, err, data);
-// 	    //callback(err, err, data);
-// 	});
+
+// request("https://www.googleapis.com/calendar/v3/calendars/chrisgervang%40gmail.com/events?maxAttendees=1&singleEvents=true&timeMin=2014-01-25T0%3A00%3A00.118z&fields=description%2Citems(location%2Cstart%2Csummary)&key={AIzaSyCByn2U9c5YUNuV3dsv0waSOxSFWpezzt8}", function(error, response, body) {
+//   console.log(body);
+// });
