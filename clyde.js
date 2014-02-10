d3e1a6d19dd8f04de72353f192c71bf3d9f1a45f
Chris Gervang <gervangster@live.com>
2014-02-09 16:08:18 -0800

    ClydE room theree

---
+++ clyde.js
@@ -0,0 +1,41 @@
+var clicks = 10;  // Amount of click until "time to go!"
+
+simply.body("", true);  // Clears initial screen
+simply.title('        CLydE');  // Initialize "CLydE" title
+var hour = 8;
+var minute = 35;
+
+var updateTime = function() {
+  if (hour < 10) {  
+      if (minute > 9) {
+      simply.subtitle("                                   0" + hour + ":" + minute);
+      } else {
+      simply.subtitle("                                   0" + hour + ":0" + minute);
+      }
+    } else {
+      if (minute > 9) {
+      simple.subtitle("                                     " + hour + ":" + minute);
+      } else {
+      simple.subtitle("                                     " + hour + ":0" + minute);
+      }
+    }
+    return;
+};  // updates time maintaining format
+
+var checkAction = function() {
+    if (Math.abs(minute - time.getMinutes()) < clicks) {
+      updateTime();
+    } else {
+      simply.subtitle(" it's time to go!");
+    }
+    return;
+};  // Determins whether to update time or announce "Time to go!" 
+
+
+updateTime();  // Initialize time
+
+  setTimeout(function(){
+    minute++;
+  checkAction();
+  )},1000);
+  
